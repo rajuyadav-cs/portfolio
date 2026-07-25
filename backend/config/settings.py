5f4d5ddbf4c319ece26f73c218acb468f09302d8
rajuyadav-cs <rajuyadav242625@gmail.com>
2026-07-25 21:13:18 +0530

    cloudinary

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'portfolio',
     'contact',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    "cloudinary",
+    "cloudinary_storage",
 ]
 
 MIDDLEWARE = [
@@ -137,5 +139,11 @@ CSRF_TRUSTED_ORIGINS = [
     if origin
 ]
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
