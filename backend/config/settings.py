b189fb03fc31ea733410d5c67930834e41805cf8
rajuyadav-cs <rajuyadav242625@gmail.com>
2026-07-25 22:10:12 +0530

    frontend-update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -145,5 +145,11 @@ CLOUDINARY_STORAGE = {
     "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
 }
 
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
